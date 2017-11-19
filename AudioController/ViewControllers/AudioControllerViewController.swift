eb4d3cb507ccc526121a0344777487f334141670
Burhan Dewantara <burhan.dewantara@gmail.com>
2017-11-19 21:59:26 +0700

    add Carthage file, add Swinject and RxSwift

---
+++ AudioController/ViewControllers/AudioControllerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AudioControllerViewController.swift
+//  AudioController
+//
+//  Created by Admin on 11/11/17.
+//  Copyright © 2017 newbeeCorp. All rights reserved.
+//
+
+import UIKit
+
+class AudioControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AudioController/ViewModels/AudioControllerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AudioControllerViewModel.swift
+//  AudioController
+//
+//  Created by Admin on 11/11/17.
+//  Copyright © 2017 newbeeCorp. All rights reserved.
+//
+
+import UIKit
+
+class AudioControllerViewModel: NSObject {
+
+}
